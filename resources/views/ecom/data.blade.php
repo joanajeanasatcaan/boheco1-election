69edd283982f73d6d6522e7cba6b9900102598e0
joanajeanasatcaan <joanajeanasatacaan@gmail.com>
2026-01-14 16:15:08 +0800

    nvm

--- resources/views/ecom/data.blade.php
+++ resources/views/ecom/data.blade.php
@@ -1,8 +1,8 @@
 <x-ecom-layout>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-6">
         <div class="bg-white shadow-md rounded-lg overflow-hidden">
-            <table class="min-w-full p border border-gray-200">
-                <thead class="bg-green-200">
+            <table class="min-w-full p border border-gray-300">
+                <thead class="bg-green-100">
                     <tr>
                         <th class="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">Profile</th>
                         <th class="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">Name</th>
